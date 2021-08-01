28f4cdac2ccd7450609ee949e8d780387db1b42b
Dmitrii Denisov <dmitriyd2009@yandex.ru>
2021-08-01 13:54:59 +0400

    адаптив страницы Contact

--- #src/js/main.js
+++ #src/js/main.js
@@ -26,13 +26,55 @@ $(function(){
         slidesToScroll: 10,
         dots:true,
         arrows: false,
+        responsive: [
+            {
+              breakpoint: 1701,
+              settings: {
+                slidesToShow: 7,
+                slidesToScroll: 7,
+              }
+            },
+            {
+                breakpoint: 1511,
+                settings: {
+                  slidesToShow: 7,
+                  slidesToScroll: 7,
+                }
+              },
+              {
+                breakpoint: 1201,
+                settings:{
+                  slidesToShow: 4,
+                  slidesToScroll: 4,
+                }
+              },
+              {
+                breakpoint: 841,
+                settings:{
+                  slidesToShow: 3,
+                  slidesToScroll: 3,
+                }
+              },
+              {
+                breakpoint: 551,
+                settings:{
+                  slidesToShow: 2,
+                  slidesToScroll: 2,
+                }
+              },
+              {
+                breakpoint: 376,
+                settings:{
+                  slidesToShow: 1,
+                  slidesToScroll: 1,
+                }
+              },
+        ]
         // loop: true,
     });
     
     $('.article-slider__box').slick({
         fade: true,
-        // dots:false,
-        // arrows: true,
         loop: true,
         prevArrow: '<button class="article-slider__arrow article-slider__arrowLeft"  type="button" ><img src="img/icons/back.svg" alt=""></img></button>',
         nextArrow: '<button class="article-slider__arrow article-slider__arrowRight"  type="button" style=""><img src="img/icons/next.svg" alt=""></img></button>',
